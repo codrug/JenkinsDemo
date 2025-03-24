e4c8da2df496778d971638b6b1b566794a96128c
Dhruv Reddy G. <132668803+codrug@users.noreply.github.com>
2025-03-24 12:36:54 +0530

    Add files via upload

---
+++ cmdlinename.java
@@ -0,0 +1,10 @@
+class cmdlinename{
+	public static void main(String [] args){
+		int name = String(arg[0]);
+		int rno = String(arg[1]);
+		int course = String(arg[2]);
+		System.out.println("Hello " + name);
+		System.out.println("Roll number: " + rno);
+		System.out.println("Course: " + course);
+	}
+}
