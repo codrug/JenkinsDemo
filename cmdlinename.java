d42d74e5f543896cc5fb198598ba8fa35bb8d894
Dhruv Reddy G. <132668803+codrug@users.noreply.github.com>
2025-03-24 12:43:58 +0530

    Update cmdlinename.java

--- cmdlinename.java
+++ cmdlinename.java
@@ -1,8 +1,8 @@
 class cmdlinename{
 	public static void main(String [] args){
-		String name = arg[0];
-		String rno = arg[1];
-		String course = arg[2];
+		String name = args[0];
+		String rno = args[1];
+		String course = args[2];
 		System.out.println("Hello " + name);
 		System.out.println("Roll number: " + rno);
 		System.out.println("Course: " + course);
