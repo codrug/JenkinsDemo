a4a912549072bffbbe0ddaafb1d53efb25e32052
Dhruv Reddy G. <132668803+codrug@users.noreply.github.com>
2025-03-25 12:10:52 +0530

    Update appletdemo01.java

--- appletdemo01.java
+++ appletdemo01.java
@@ -1,12 +1,12 @@
 import java.applet.*;
 import java.awt.*;
+
+/*<applet code= "appletdemo01.class" width="300" height="300"></applet>*/
 public class appletdemo01 extends Applet{
   public void paint(Graphics g){
     g.drawString("Hello, World!", 100, 150);
   }
 }
-
-/*<applet code= "appletdemo01.class" width="300" height="300"></applet>*/
 /*
   javac filename.java
   appletviewer filename.java
